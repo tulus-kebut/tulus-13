7b5467cac4b948960995212118220e805776b3c5
Lalu Tulus Dwi Sapte <117416040+tulus-kebut@users.noreply.github.com>
2023-09-29 21:44:02 +0800

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="gg.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hasil</title>
+</head>
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    echo "Nama: $name<br>";
+    echo "Email: $email<br>";
+} else {
+    echo "Metode permintaan yang diperbolehkan adalah POST.";
+}
+?>
+</body>
+</html>
+
